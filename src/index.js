881d543f2df82978ede7329d817563c74c8ab021
Michael Campbell <michael.campbell14@gmail.com>
2020-03-13 16:03:50 -0400

    learned how to do inline css

--- src/index.js
+++ src/index.js
@@ -2,23 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-function MyApp() {
+function MyInfo() {
     return (
-        <ul>
-            <li>this is number 1</li>
-            <li>this is number 2</li>
-            <li>this is number 3</li>
+        <div>
+            <h1 style={{color: "blue"}}>Michael Campbell</h1>
+            <p>This is me writing a paragraph using ReactJS!</p>
+            <ul>
+                <li>Tokyo</li>
+                <li>Portugal</li>
+                <li>Australia</li>
             </ul>
+        </div>
     )
 };
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<MyInfo />, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
